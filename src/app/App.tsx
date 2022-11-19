be572d88aa96ef234aa59f3c5d37e21122a1b7a6
isqua <isqua@isqua.ru>
2022-11-19 16:03:54 +0300

    chore: remove extra content and styles from App

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './App.module.css';
+
+export function App(): React.ReactElement {
+  return (
+    <div className={styles.app}>
+      hello, world!
+    </div>
+  );
+}
